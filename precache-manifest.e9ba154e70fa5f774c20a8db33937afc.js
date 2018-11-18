92c0197c26bc5771489f70f01a015a1bbf800576
divyang duhan <divyang.duhan07@gmail.com>
2018-11-18 02:58:27 -0600

    Updates

---
+++ precache-manifest.e9ba154e70fa5f774c20a8db33937afc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "26bfecd7ef28da80ef12c7f8c7e947cf",
+    "url": "/portfolio/static/media/Niveau Grotesk Medium.26bfecd7.otf"
+  },
+  {
+    "revision": "f7211ce71c9e133df928",
+    "url": "/portfolio/static/css/main.00ec720a.chunk.css"
+  },
+  {
+    "revision": "0e04ef5a516e40bab9900012cc6a65b4",
+    "url": "/portfolio/static/media/header.0e04ef5a.less"
+  },
+  {
+    "revision": "ba8d57ad9582251ade8b",
+    "url": "/portfolio/static/js/1.ba8d57ad.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c10e062d79e5b7484cef1038ca8e39e2",
+    "url": "/portfolio/static/media/sheores.c10e062d.png"
+  },
+  {
+    "revision": "f7211ce71c9e133df928",
+    "url": "/portfolio/static/js/main.f7211ce7.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b212fa73095eccca110c6447476bf221",
+    "url": "/portfolio/static/media/footer.b212fa73.less"
+  },
+  {
+    "revision": "912fb84e0f786dca8dbbfbe93d1ed44b",
+    "url": "/portfolio/static/media/card.912fb84e.less"
+  },
+  {
+    "revision": "5484c148e0f3b04eb9236ce3893192b4",
+    "url": "/portfolio/static/media/timeline.5484c148.less"
+  },
+  {
+    "revision": "ba8d57ad9582251ade8b",
+    "url": "/portfolio/static/css/1.d0addfa0.chunk.css"
+  },
+  {
+    "revision": "58ae30218e77997fed8c99655cdc1eab",
+    "url": "/portfolio/index.html"
+  }
+];
